07a64abf2bb67823249dfc06e76fcf4476497060
r06921017 <r06921017@ntu.edu.tw>
2020-04-01 00:57:40 -0700

    Rotate the frame to 90 degree.

--- src/node.cpp
+++ src/node.cpp
@@ -30,7 +30,7 @@ vector<string> joint_names({"SPINEBASE", "SPINEMID", "NECK", "HEAD", "SHOULDERLE
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "nturgbd_skeleton_node");
+	ros::init(argc, argv, "hri_costmap_node");
 	ros::NodeHandle nh;
 	ros::NodeHandle p_nh("~");
 	ros::Rate r(10);
@@ -47,21 +47,21 @@ int main(int argc, char** argv)
 	string camera_frame_id = p_nh.param(string("camera_frame_id"), string("camera"));
 	
 	static tf2_ros::TransformBroadcaster br;
-	ros::Publisher skeleton_marker_array_pub = nh.advertise<visualization_msgs::MarkerArray>("/nturgbd_skeleton_viz", 10);
+	ros::Publisher skeleton_marker_array_pub = nh.advertise<visualization_msgs::MarkerArray>("/hri_costmap_viz", 10);
 
 	vector<geometry_msgs::TransformStamped> body_transforms(50); //25 per person and maximum 2 people in a frame
 	visualization_msgs::MarkerArray marker_array;
 	
-	for(int i=0;i<2;i++)
-		for(int j=0;j<25;j++)
+	for(int i = 0; i < 2; i ++)
+		for(int j = 0; j < 25; j ++)
 		{
 			int index = i*25 + j;
 			body_transforms[index].header.seq = 0;
 			body_transforms[index].header.frame_id = camera_frame_id;
-			body_transforms[index].child_frame_id = "nturgbd_skeleton_"+to_string(i)+"_"+joint_names[j];
+			body_transforms[index].child_frame_id = "hri_costmap_"+to_string(i)+"_"+joint_names[j];
 
 			visualization_msgs::Marker marker;
-			marker.ns = "nturgbd_skeleton";
+			marker.ns = "hri_costmap";
 			marker.id = index;
 			marker.lifetime = ros::Duration(0.5);
 			marker.header.stamp = ros::Time();
@@ -104,13 +104,13 @@ int main(int argc, char** argv)
 			return -1;
 		}
 
-		for(int frame=0;frame<frame_count;frame++)
+		for(int frame = 0; frame < frame_count; frame ++)
 		{
 			int body_count;
-			skeleton_file>>body_count; //no. of observerd skeletons in current frame
+			skeleton_file >> body_count; // no. of observed skeletons in current frame
 			
 			ros::Time t = ros::Time::now();
-			for(int body_num=0;body_num<body_count;body_num++)
+			for(int body_num = 0; body_num < body_count; body_num ++)
 			{
 				long int bodyID;
 				skeleton_file >> bodyID;
@@ -127,7 +127,7 @@ int main(int argc, char** argv)
 				int joint_count;
 				skeleton_file >> joint_count;
 
-				for(int joint_num=0; joint_num < joint_count; joint_num++)
+				for(int joint_num = 0; joint_num < joint_count; joint_num ++)
 				{
 					float x, y, z, depthX, depthY, colorX, colorY, orientationW, orientationX, orientationY, orientationZ;
 					skeleton_file >> x >> y >> z >> depthX >> depthY >> colorX >> colorY >> orientationW >> orientationX >> orientationY >> orientationZ;

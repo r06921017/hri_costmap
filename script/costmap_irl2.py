dd35e44f99b0fac055823b26ca3eeca743bab823
r06921017 <r06921017@ntu.edu.tw>
2020-05-07 08:57:15 +0800

    Add precision as a variable.

--- script/costmap_irl2.py
+++ script/costmap_irl2.py
@@ -5,6 +5,7 @@ import os
 import gym
 import time
 import typing
+import logging
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -19,11 +20,13 @@ from util.dataset import Dataset, transition
 
 class CostmapIRL:
     def __init__(self):
-        self.csv_path = '/home/rdaneel/hri_costmap_traj/actions/'
+        self.csv_path = '/home/rdaneel/hri_costmap_traj/A107_hp/actions/'
         self.action_success_rate = 1.0
         self.df_list = list()
         self.file_name = list()
         self.goal_states = list()
+        self.precision = 0.01
+        self.around_digit = len(str(self.precision).split('.'))
 
         for _fin in os.listdir(self.csv_path):
             self.df_list.append(pd.read_csv(self.csv_path + _fin))
@@ -46,10 +49,10 @@ class CostmapIRL:
             tmp_a1.append(np.nanmax(_df['a1'].tolist()))
             tmp_a2.append(np.nanmax(_df['a2'].tolist()))
 
-        self.max_f1 = np.around(np.nanmax(tmp_f1), 1)
-        self.max_f2 = np.around(np.nanmax(tmp_f2), 1)
-        self.max_a1 = np.around(np.nanmax(tmp_a1), 1)
-        self.max_a2 = np.around(np.nanmax(tmp_a2), 1)
+        self.max_f1 = np.around(np.nanmax(tmp_f1), self.around_digit)
+        self.max_f2 = np.around(np.nanmax(tmp_f2), self.around_digit)
+        self.max_a1 = np.around(np.nanmax(tmp_a1), self.around_digit)
+        self.max_a2 = np.around(np.nanmax(tmp_a2), self.around_digit)
 
         # Get the minimum position among all demos
         tmp_f1 = list()
@@ -62,20 +65,20 @@ class CostmapIRL:
             tmp_a1.append(np.nanmin(_df['a1'].tolist()))
             tmp_a2.append(np.nanmin(_df['a2'].tolist()))
 
-        self.min_f1 = np.around(np.nanmin(tmp_f1),1)
-        self.min_f2 = np.around(np.nanmin(tmp_f2),1)
-        self.min_a1 = np.around(np.nanmin(tmp_a1),1)
-        self.min_a2 = np.around(np.nanmin(tmp_a2),1)
+        self.min_f1 = np.around(np.nanmin(tmp_f1),self.around_digit)
+        self.min_f2 = np.around(np.nanmin(tmp_f2),self.around_digit)
+        self.min_a1 = np.around(np.nanmin(tmp_a1),self.around_digit)
+        self.min_a2 = np.around(np.nanmin(tmp_a2),self.around_digit)
 
         self.f1_idx = dict()
-        for i, f in enumerate(np.arange(self.min_f1, self.max_f1 + 0.05, 0.1)):
-            self.f1_idx[i] = np.around(f, 1)
+        for i, f in enumerate(np.arange(self.min_f1, self.max_f1 + self.precision/2.0, self.precision)):
+            self.f1_idx[i] = np.around(f, self.around_digit)
         self.f1_length = len(self.f1_idx)
         self.inv_f1_idx = {_tmp_v: _tmp_k for _tmp_k, _tmp_v in self.f1_idx.items()}
 
         self.f2_idx = dict()
-        for i, f in enumerate(np.arange(self.min_f2, self.max_f2 + 0.05, 0.1)):
-            self.f2_idx[i] = np.around(f, 1)
+        for i, f in enumerate(np.arange(self.min_f2, self.max_f2 + self.precision/2.0, self.precision)):
+            self.f2_idx[i] = np.around(f, self.around_digit)
         self.f2_length = len(self.f2_idx)
         self.inv_f2_idx = {_tmp_v: _tmp_k for _tmp_k, _tmp_v in self.f2_idx.items()}
 
@@ -90,10 +93,12 @@ class CostmapIRL:
         print('state space size: {0}'.format(self.state_size))
 
         self.action_idx = dict()
-        for i, a1 in enumerate(np.arange(self.min_a1, self.max_a1 + 0.05, 0.1)):
-            for j, a2 in enumerate(np.arange(self.min_a2, self.max_a2 + 0.1, 0.1)):
-                a_id = i*len(np.arange(self.min_a2, self.max_a2 + 0.05, 0.1)) + j
-                self.action_idx[a_id] = (np.around(a1, 1), np.around(a2, 1))
+        for i, a1 in enumerate(np.arange(self.min_a1, self.max_a1 + self.precision/2.0, self.precision)):
+            for j, a2 in enumerate(np.arange(self.min_a2, self.max_a2 + self.precision/2.0, self.precision)):
+                a_id = i*len(np.arange(self.min_a2, self.max_a2 + self.precision/2.0, self.precision)) + j
+                self.action_idx[a_id] = (np.around(a1, self.around_digit), np.around(a2, self.around_digit))
+
+        print('action space size: {0}'.format(len(self.action_idx)))
 
         self.inv_action_idx = {_tmp_v: _tmp_k for _tmp_k, _tmp_v in self.action_idx.items()}
 
@@ -185,30 +190,72 @@ class CostmapIRL:
     
     def collect_demo(self):        
         dataset = Dataset(self.max_length)
-        for _df in self.df_list:
+        for idx, _df in enumerate(self.df_list):
             t = list()
             for idx in range(_df.shape[0]-1):  # before termination?
                 t.append(transition(
-                    obs=self._pos2state(np.around(_df['f1'][idx], 1), np.around(_df['f2'][idx], 1)), 
-                    act=self.inv_action_idx[(np.around(_df['a1'][idx+1], 1), np.around(_df['a2'][idx+1], 1))],
-                    next_obs=self._pos2state(np.around(_df['f1'][idx+1], 1), np.around(_df['f2'][idx+1], 1)),
+                    obs=self._pos2state(np.around(_df['f1'][idx], self.around_digit), np.around(_df['f2'][idx], self.around_digit)), 
+                    act=self.inv_action_idx[(np.around(_df['a1'][idx+1], self.around_digit), np.around(_df['a2'][idx+1], self.around_digit))],
+                    next_obs=self._pos2state(np.around(_df['f1'][idx+1], self.around_digit), np.around(_df['f2'][idx+1], self.around_digit)),
                     rew=1.0))
             dataset.append(t)
         
-            self.goal_states.append(self._pos2state(np.around(_df['f1'][_df.shape[0]-1], 1), 
-                                                    np.around(_df['f2'][_df.shape[0]-1], 1)))
+            self.goal_states.append(self._pos2state(np.around(_df['f1'][_df.shape[0]-1], self.around_digit), 
+                                                    np.around(_df['f2'][_df.shape[0]-1], self.around_digit)))
 
         return dataset
                 
-    def get_boundary(self, RMatrix):
-        diff = np.zeros(RMatrix.shape[1])
+    def get_boundary(self, RMatrix, threshold=1.0):
+        diff = np.zeros((RMatrix.shape[0], RMatrix.shape[1]))
         for i in range(1, RMatrix.shape[1]):
-            print('R[{0}] = {1}'.format(i, RMatrix[1, i]))
-            diff[i] = abs(RMatrix[1, i] - RMatrix[1, i-1])
+            diff[:, i] = RMatrix[:, i] - RMatrix[:, i-1]
 
-        print(diff)
-        return np.argmax(diff)
+        diff -= np.ones((RMatrix.shape[0], RMatrix.shape[1])) * threshold
 
+        # print(diff)
+        tmp_f2_idx, tmp_f1_idx = np.where(diff > 0)
+        print(tmp_f2_idx)
+        print(tmp_f1_idx)
+
+        return np.min(tmp_f1_idx)
+
+    @staticmethod
+    def readFile(f_name):
+        if os.path.exists(f_name):
+            return pd.read_csv(f_name)
+        else:
+            logging.error('{0} does not exist!'.format(f_name))
+            exit(1)
+
+    @staticmethod
+    def getDist(x1, y1, z1, x2, y2, z2):
+        return np.sqrt((pow((x1-x2),2) + pow((y1-y2),2) + pow((z1-z2),2)))
+
+    def extract_features(self, raw_data_path='/home/rdaneel/hri_costmap_traj/'):
+        for _fin in os.listdir(raw_data_path):
+            if _fin.split('.')[-1] == 'csv':
+                file_name = raw_data_path + _fin
+                traj_df = readFile(file_name)
+                h_id = '1'
+                r_id = '2'
+
+                goal_pos2 = [traj_df['x'+r_id].tolist()[-1], traj_df['y'+r_id].tolist()[-1], traj_df['z'+r_id].tolist()[-1]]
+                for idx, row in traj_df.iterrows():
+                    traj_df.loc[idx, 'f1'] = np.around(getDist(row['x'+r_id], row['y'+r_id], row['z'+r_id], row['x'+h_id], row['y'+h_id], row['z'+h_id]), self.around_digit)
+                    traj_df.loc[idx, 'f2'] = np.around(getDist(row['x'+r_id], row['y'+r_id], row['z'+r_id], goal_pos2[0], goal_pos2[1], goal_pos2[2]), self.around_digit)
+
+                pre_row = pd.Series()
+                for idx, row in traj_df.iterrows():
+                    if idx == 0:
+                        pre_row = row
+                    else:
+                        traj_df.loc[idx, 'a1'] = row['f1']-pre_row['f1']
+                        traj_df.loc[idx, 'a2'] = row['f2']-pre_row['f2']
+                        pre_row = row
+                        
+                traj_df.to_csv(raw_data_path + 'actions/' + _fin.split('.')[0] + '_action.csv')
+
+    
     def main(self):
         np.random.seed(0)
         
@@ -219,8 +266,10 @@ class CostmapIRL:
         print('Collect dataset ...')
         in_dataset = self.collect_demo()
         plot_dataset_distribution(in_dataset, (self.f1_length, self.f2_length), "Dataset State Distribution")
+        print('Done!')
 
         # IRL
+        print('Building IRL object ...')
         me_irl = MaxEntIRL(
             observation_space=self.observation_space(),
             action_space=self.action_space(),
@@ -230,13 +279,15 @@ class CostmapIRL:
             feature_map=phi,
             max_iter=20,
             anneal_rate=0.9)
+        print('Done!')
 
-        print('Start training ...')
+        print('\nStart training ...')
         Rprime = me_irl.train()
         Rprime = Rprime.reshape((self.f1_length, self.f2_length)).T
 
         print('Boundary index = ', self.get_boundary(Rprime))
         print('Radius = ', self.f1_idx[self.get_boundary(Rprime)])
+        print(self.f1_idx)
 
         # plot results
         plot_grid_map(Rprime, "Reward (IRL)", print_values=True, cmap=plt.cm.Blues)
--- script/get_action2.py
+++ script/get_action2.py
@@ -18,17 +18,19 @@ def getDist(x1, y1, z1, x2, y2, z2):
 
 
 if __name__ == '__main__':
-    for _fin in os.listdir('/home/rdaneel/hri_costmap_traj'):
+    around_digit = 2
+    
+    for _fin in os.listdir('/home/rdaneel/hri_costmap_traj/A107/'):
         if _fin.split('.')[-1] == 'csv':
-            file_name = '/home/rdaneel/hri_costmap_traj/' + _fin
+            file_name = '/home/rdaneel/hri_costmap_traj/A107/' + _fin
             traj_df = readFile(file_name)
             h_id = '1'
             r_id = '2'
 
             goal_pos2 = [traj_df['x'+r_id].tolist()[-1], traj_df['y'+r_id].tolist()[-1], traj_df['z'+r_id].tolist()[-1]]
             for idx, row in traj_df.iterrows():
-                traj_df.loc[idx, 'f1'] = np.around(getDist(row['x'+r_id], row['y'+r_id], row['z'+r_id], row['x'+h_id], row['y'+h_id], row['z'+h_id]), 1)
-                traj_df.loc[idx, 'f2'] = np.around(getDist(row['x'+r_id], row['y'+r_id], row['z'+r_id], goal_pos2[0], goal_pos2[1], goal_pos2[2]), 1)
+                traj_df.loc[idx, 'f1'] = np.around(getDist(row['x'+r_id], row['y'+r_id], row['z'+r_id], row['x'+h_id], row['y'+h_id], row['z'+h_id]), around_digit)
+                traj_df.loc[idx, 'f2'] = np.around(getDist(row['x'+r_id], row['y'+r_id], row['z'+r_id], goal_pos2[0], goal_pos2[1], goal_pos2[2]), around_digit)
 
             pre_row = pd.Series()
             for idx, row in traj_df.iterrows():
